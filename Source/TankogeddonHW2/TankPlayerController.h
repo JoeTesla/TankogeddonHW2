fc99fcae959cf44ef798ab9d1ac5c5da373b24d9
JoeTesla <joean@mail.ru>
2021-08-30 22:46:21 +0300

    Added CSC and MoveForward

--- Source/TankogeddonHW2/TankPlayerController.h
+++ Source/TankogeddonHW2/TankPlayerController.h
@@ -6,12 +6,28 @@
 #include "GameFramework/PlayerController.h"
 #include "TankPlayerController.generated.h"
 
+class ATankPawn;
+
 /**
- * 
+ *
  */
+
 UCLASS()
+
 class TANKOGEDDONHW2_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+
+protected:
+	UPROPERTY()
+		ATankPawn* TankPawn;
+
+public:
+	ATankPlayerController();
+	virtual void SetupInputComponent() override;
+
+protected:
+	virtual void BeginPlay() override;
+	void MoveForward(float AxisValue);
+
 };
